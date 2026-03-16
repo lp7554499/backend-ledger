1547f09453c866756c7144e3d4ef83b1bb9ea5fc
lp7554499 <lp7554499@gmail.com>
2026-03-16 22:30:21 +0530

    name & password add Schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,9 +4,19 @@ const userSchema = mongoose.Schema({
     email:{
         type: String,
         required:[true, 'Email is required'],
-        unique: true,
-        trim,
+        unique: [true, 'Email already exists'],
+        trim: true,
         lowercase: true,
         match: [/.+\@.+\..+/, 'Please fill a valid email address']
+    },
+    name:{
+        type: String,
+        required:[true, 'Name is required'],
+        trim: true
+    },
+    password:{
+        type: String,
+        required:[true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters long']
     }
 })
